e00bc057dc301653c39b698bf2eff1396914f96c
PepsiColaS <workmail912@mail.ru>
2024-07-06 14:21:42 +0300

    'dfgdfgdfg'

--- src/config.js
+++ src/config.js
@@ -25,25 +25,6 @@ const Loginschema = new mongoose.Schema({
     }
 });
 
-
-
-// const Courseschema = new mongoose.Schema({
-//     TeacherName: {
-//         type: String,
-//         required: true
-//     },
-//     Title: {
-//         type: String,
-//         required: true
-//     },
-//     students: {
-//         type: String,
-//         required: true
-//     }
-// });
-
-// collection part
 const collection = new mongoose.model("users", Loginschema);
-// const collectionCourse = new mongoose.model("course", Courseschema);
 module.exports = collection;
-// module.exports = collectionCourse;
+
--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 const connect = mongoose.connect("mongodb://localhost:27017/Zachetka");
 const bodyParser = require('body-parser');
-
 const express = require("express");
 const path = require("path");
 const collection = require("./config");
-
 const app = express();
+
+let studId
 
 app.use(bodyParser.json());
 
-
 const { Schema } = mongoose;
 const textSchema = new Schema({
-  text: String
+  text: String,
+  students: []
 });
 const TextModel = mongoose.model('courses', textSchema);
-
-
+ 
 // convert data into json format
 app.use(express.json());
 // Static file
@@ -28,17 +27,81 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 
-
 // Маршрут для получения данных
 app.get('/home', async (req, res) => {
     try {
-      const data = await TextModel.find(); // Замените YourModel на ваш Mongoose model
-      res.send(data);
+        let data = await TextModel.find().lean();
+        let htmlOutput = '';
+        data.forEach(item => {
+            htmlOutput += 
+            `<form action="/enroll/${item._id}" method="post">
+                <input type="hidden" name="courseId" value="${item._id}">
+                <h1>Название курса: ${item.text}</h1>
+                <h2>ФИО преподавателя: Беднякова Татьяна Михайловна</h2> 
+                <button type="submit">Записаться на курс</button>
+            </form>`
+            ;
+        });
+        res.send(htmlOutput);
     } catch (err) {
-      res.status(500).send(err.message);
+        res.status(500).send(err.message);
+    }
+});
+
+app.post('/enroll/:id', async (req, res) => {
+    try {
+        const courseId = req.params.id;
+        const studentId = studId;
+        const result = await TextModel.updateOne(
+            { _id: courseId },
+            { $push: { students: studentId } }
+        );
+
+        res.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Ошибка на сервере' });
+    }
+});
+
+app.get('/teacher', async (req, res) => {
+    try {
+        let data = await TextModel.find().lean();
+        let htmlOutput = '';
+        data.forEach(item => {
+            htmlOutput += 
+                `<form action="/bigmak/${item._id}" method="post">
+                    <h1>Название курса: ${item.text}</h1>
+                    <h2>ФИО преподавателя: Беднякова</h2>
+                    <button id="${item._id}">Список студентов</button>
+                </form>
+                `;
+        });
+        htmlOutput += `<form action="/addText" method="POST">
+        <input type="text" name="text" placeholder="Введите текст">
+        <button type="submit">Добавить текст</button>
+        </form>`
+
+        res.send(htmlOutput);
+    } catch (err) {
+        res.status(500).send(err.message);
     }
     
-  });
+});
+
+app.post('/bigmak/:id', async (req, res) => {
+    try {
+        const courseId = req.params.id;
+        const result = await TextModel.findOne({courseId})
+
+        console.log(result.students)
+
+        res.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: 'Ошибка на сервере' });
+    }
+});
 
 
 app.get("/", (req, res) => {
@@ -59,29 +122,22 @@ app.get("/signup", (req, res) => {
 
 // Register User
 app.post("/signup", async (req, res) => {
-
     const data = {
         name: req.body.username,
         password: req.body.password,
         role: 1
     }
-
     const existingUser = await collection.findOne({ name: data.name });
 
     if (existingUser) {
         res.send('User already exists. Please choose a different username.');
     } else {
-
         const userdata = await collection.insertMany(data);
         res.render('login')
         // console.log(userdata);
     }
 
 });
-
-
-
-
 
 // Login user 
 app.post("/login", async (req, res) => {
@@ -96,6 +152,7 @@ app.post("/login", async (req, res) => {
         }
         else { 
              if (check.role == '1'){
+                studId = check._id
                 res.redirect('home')
              }
              else{
@@ -107,11 +164,6 @@ app.post("/login", async (req, res) => {
         res.send("wrong Details");
     }
 });
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
   
 app.post('/addText', async (req, res) => {
         const data = {
